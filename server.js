3df23c52de9d2d49f4e8dccd636190e12f9dd9c3
ama-ali <ama-ali@hotmail.co.uk>
2023-05-14 20:57:20 +0100

    server.js file added

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+//Dependencies
 const express = require("express");
 const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes');
+
+//server setup
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+// json file parsed middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+// handle the static files public path directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use('/api/notes', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.get('/', (req, res) => {
+  res.send('root')
+})
 
 
-app.listen(PORT, () => {
-    console.log(`API server listening on port http://localhost:${PORT}.`);
+app.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}.`);
   });
